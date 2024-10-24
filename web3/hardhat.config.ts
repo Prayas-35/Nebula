c405bf4ed5d50f83911740bb63510b73114707cc
Prayas <prayaspal04@gmail.com>
2024-10-24 17:38:24 +0530

    aia chain address

--- web3/hardhat.config.ts
+++ web3/hardhat.config.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-verify";
 
-const { RPC_URL_ETH, RPC_URL_BASE, RPC_URL_AMOY, PRIVATE_KEY, ETHERSCAN_API, POLYGONSCAN_API, BASESCAN_API } = process.env;
+const { RPC_URL_ETH, RPC_URL_BASE, RPC_URL_AMOY, RPC_URL_AIA, PRIVATE_KEY, ETHERSCAN_API, POLYGONSCAN_API, BASESCAN_API } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
@@ -20,12 +20,17 @@ const config: HardhatUserConfig = {
       url: RPC_URL_BASE || "",
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
     },
+    aia: {
+      url: RPC_URL_AIA || "",
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
   },
   etherscan: {
     apiKey: {
       'base-sepolia': `${ETHERSCAN_API}`,
       'polygonAmoy': `${POLYGONSCAN_API}`,
       'sepolia': `${BASESCAN_API}`,
+      'aia': `${ETHERSCAN_API}`,
     },
     customChains: [
       {
@@ -34,6 +39,14 @@ const config: HardhatUserConfig = {
         urls: {
           apiURL: "https://base-sepolia.blockscout.com/api",
           browserURL: "https://base-sepolia.blockscout.com"
+        }
+      },
+      {
+        network: "aia",
+        chainId: 1320,
+        urls: {
+          apiURL: "https://testnet.aiascan.com/api",
+          browserURL: "https://testnet.aiascan.com"
         }
       }
     ]
